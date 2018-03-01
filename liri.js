ab63a6770420d347e7a4bb30d20094c6844b710b
unknown <marcushilaire@gmail.com>
2018-03-01 17:09:35 -0500

    dependencies

--- liri.js
+++ liri.js
@@ -1,3 +1,5 @@
-// require("dotenv").config();
-var keys = require("./keys.js")
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+require("dotenv").config();
+var keys = require("./keys.js");
 console.log(keys)
