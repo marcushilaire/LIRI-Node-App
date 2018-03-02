1459c72f2658e350244163e02e798813fa96b095
unknown <marcushilaire@gmail.com>
2018-03-01 20:02:34 -0500

    removing comments

--- liri.js
+++ liri.js
@@ -10,21 +10,6 @@ var spotify = new Spotify(keys.spotify);
 var action = process.argv[2];
 var query = process.argv[3];
 
-
-// // console.log(spotifyKeys);
-// // console.log("=======")
-// // console.log(twitterKeys);
-// // fs.readFile("random.txt", "utf8", function(error, data) {
-// //     if (error) {
-// //       return console.log(error);
-// //     }
-// //     console.log(data);
-// //     });
-// client.post('statuses/update', {status: 'I Love Twitter'},  function(error, tweet, response) {
-//     if(error) throw error;
-//     console.log(tweet);  // Tweet body. 
-//     console.log(response);  // Raw response object. 
-//     });
 if(action){
     if (action == "my-tweets"){
         client.get("statuses/user_timeline", function(error, tweets, response){
